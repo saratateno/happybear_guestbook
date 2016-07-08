b6ef155e28946c6197131c77b90bbb5effc5a7bc
Sara Tateno <sara.tateno@gmail.com>
2016-07-08 23:51:37 +0100

    Test for adding a new entry

--- spec/features/entries_feature_spec.rb
+++ spec/features/entries_feature_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
 feature 'entries' do
-  scenario   'no entries have been added' do
+  scenario 'no entries have been added' do
     visit '/'
     expect(page).to have_content 'No messages yet.'
+  end
+
+  context 'when adding a new entry' do
+    scenario 'a successful entry' do
+      fill_in('entry[name]', with: 'Joe Bloggs')
+      fill_in('entry[message]', with: 'Hello World')
+      click_button('Submit')
+      expect(page).to have_content 'Joe Bloggs'
+      expect(page).to have_content 'Hello World'
+    end
   end
 end
